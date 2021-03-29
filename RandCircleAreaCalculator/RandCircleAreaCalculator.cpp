424b358d2adc36c68f20c2da77a26d9f8d3665bf
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-03-29 11:16:27 -0500

    Formatting, spelling

--- RandCircleAreaCalculator/RandCircleAreaCalculator.cpp
+++ RandCircleAreaCalculator/RandCircleAreaCalculator.cpp
@@ -14,13 +14,12 @@ using namespace std; //get rid of having to write std::FUNCTION
 size_t NThrows = 100000; //NThrows = # of throws/random numbers
 
 
-int main()
-{
+int main() {
 	srand(time(NULL)); //Seed the random number generator
 	for (int j = 1; j <= 40; j++) {
 		size_t NHits = 0, NMisses = 0;
 		NThrows *= 10;
-#       pragma omp parallel for reduction(+ : NHits) reduction(+ : NMisses)
+#pragma omp parallel for reduction(+ : NHits) reduction(+ : NMisses)
 		for (long long int i = 0; i < NThrows; i++) {
 			long double xi = static_cast<long double>(rand()) / RAND_MAX; //Generate positive x-value range:[0,1]
 			long double yi = static_cast<long double>(rand()) / RAND_MAX; //Generate positive y-value range:[0,1]
@@ -38,7 +37,9 @@ int main()
 		}
 		long double HitTotal = static_cast<long double>(NHits) / static_cast<long double>(NThrows); //Compute Hits/Total thrown
 		long double approxPi = HitTotal * 4; //Determine area of circle by multiplying HitTotal by area of square
-		printf("Try %2d: %18.16f  error = %10.3e\%\n",
-			j, approxPi, 100*abs((1 - approxPi / M_PI)));
+		printf(
+			"Try %2d: %18.16f  error = %10.3e\%\n",
+			j, approxPi, 100*abs((1 - approxPi / M_PI))
+		);
 	}
 }
